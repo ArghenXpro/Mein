fd52d99a62346d0d33eeee5017d0df117dc0f988
ArghenXpro <argenjoldubaew84@gmail.com>
2024-04-11 14:43:19 +0600

    Final

--- main.js
+++ main.js
@@ -26,27 +26,37 @@ searchInputs.forEach(search => {
 
 const me = document.querySelector('.me'); // Используем querySelector для выбора первого элемента с классом 'me'
 const about = document.querySelector('.about'); // Используем querySelector для выбора первого элемента с классом 'about'
-const btn = document.querySelector('button');
+const btn = document.querySelector('#show');
 const con = document.querySelector('.container')
 me.addEventListener('click', () => {
     about.classList.toggle('hidden');
-    btn.classList.toggle('move'); // Добавляем или удаляем класс 'move' при каждом клике
+ // Добавляем или удаляем класс 'move' при каждом клике
 });
 btn.addEventListener('click', () => {
-    btn.classList.toggle('move'); // Добавляем или удаляем класс 'move' при каждом клике на кнопку
+     // Добавляем или удаляем класс 'move' при каждом клике на кнопку
     con.classList.toggle('container')
 });
 
 // Находим элемент input для загрузки файла
-const uploadInput = document.querySelector('.upload input[type="file"]');
-const audioPlayer = document.getElementById('audioPlayer');
+document.addEventListener('DOMContentLoaded', function () {
+    var audioPlayer = document.getElementById('audioPlayer');
+    var uploadInput = document.querySelector('input.upload');
 
-uploadInput.addEventListener('change', function() {
-    const file = this.files[0];
-    const objectUrl = URL.createObjectURL(file); // Создаем URL для загруженного файла
-    audioPlayer.src = objectUrl; // Задаем src аудиоплееру
-    audioPlayer.play(); // Автоматически воспроизводим песню после загрузки
+    uploadInput.addEventListener('change', function (event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            audioPlayer.src = e.target.result;
+            audioPlayer.play();
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    });
 });
+
 function playAudio() {
     audioPlayer.play();
 }
@@ -61,8 +71,38 @@ function stopAudio() {
 }
 
 const btnss = document.querySelectorAll(".icon");
-   for (const btn of btnss) {
-       btn.addEventListener("click", () => {
-           btn.parentElement.classList.toggle("open");
-       });
-   }
+for (const btn of btnss) {
+    btn.addEventListener("click", () => {
+        btn.parentElement.classList.toggle("open");
+    });
+}
+document.addEventListener('DOMContentLoaded', function() {
+    var genreTitles = document.querySelectorAll('#genre-container h4');
+
+    genreTitles.forEach(function(title) {
+        title.addEventListener('click', function() {
+            var audioElement = this.nextElementSibling;
+            audioElement.classList.toggle('hidden');
+        });
+    });
+});
+    // Получаем ссылку на модальное окно и кнопку для его открытия
+    var modal = document.getElementById("loginModal");
+    var btnsss = document.getElementById("loginBtn");
+    // Получаем элемент для закрытия модального окна
+    var span = document.getElementsByClassName("close")[0];
+    // Открываем модальное окно при клике на кнопку
+    btnsss.onclick = function () {
+      modal.style.display = "block";
+    }
+    // Закрываем модальное окно при клике на крестик
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
+    // Закрываем модальное окно при клике за его пределами
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+    
